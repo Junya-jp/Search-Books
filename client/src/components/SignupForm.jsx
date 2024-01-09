141395c1abce66d43971e7dbfc99a6e9fc0f29c9
Junior Poku <juniorpoku@Juniors-MacBook-Pro.local>
2024-01-08 19:56:09 -0500

    Updated signup form using graphql mutations for apollo server

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
+//Remove this line and add new graphql mutation
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/Mutations';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -11,7 +12,7 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
-
+  const [addUser,{error, data}] =useMutation(ADD_USER);
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
@@ -28,13 +29,14 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      // Use the addUser mutation from Apollo Client
+      const { data } = await addUser({
+        variables: { ...userFormData },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      console.log(data);  
 
-      const { token, user } = await response.json();
+      const { token, user } = data.addUser;  // Update with the correct data structure
       console.log(user);
       Auth.login(token);
     } catch (err) {
