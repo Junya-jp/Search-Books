0f4a286e472643d9ef359b9a81b45735ba94f4e8
Junior Poku <juniorpoku@Juniors-MacBook-Pro.local>
2024-01-09 14:58:36 -0500

    Fixed connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
 });
 
 const db = mongoose.connection;
@@ -13,3 +11,5 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB');
 });
+
+module.exports = db;
