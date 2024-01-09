b378c20f080e40bd761fefbf1fe8caa5c4b51a61
Junior Poku <juniorpoku@Juniors-MacBook-Pro.local>
2024-01-08 21:37:49 -0500

    Vite config file changed for apollo server

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePluginGraphql } from 'vite-plugin-graphql';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginGraphql({
+      clientOptions: {
+        uri: 'http://localhost:3001/graphql', // Update with your GraphQL server URI
+        cache: new InMemoryCache(),
+      },
+    }),
+  ],
   server: {
     port: 3000,
     open: true,
     proxy: {
-      '/api': {
+      '/graphql': {
         target: 'http://localhost:3001',
         secure: false,
-        changeOrigin: true
-      }
-    }
-  }
-})
+        changeOrigin: true,
+      },
+    },
+  },
+});
