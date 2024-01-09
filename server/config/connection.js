da8e27c955ad4f0f398de192507fb9bb23cb0fb7
Junior Poku <juniorpoku@Juniors-MacBook-Pro.local>
2024-01-09 14:49:32 -0500

    Updated Connection file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect('mongodb://127.0.0.1:27017/googlebooks', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
-module.exports = mongoose.connection;
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
